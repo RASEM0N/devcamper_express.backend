f6ae5ffc6ef87b070c301297855e79540c61a105
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-02-09 18:23:58 +0500

    3.3 Настройка маршрутов

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -4,24 +4,57 @@ const dotenv = require('dotenv');
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// создаем нашm express
 const app = express();
 
-/* привзяка действий по маршруту localhost:5000/
- * или просто localhost:5000*/
-app.get('/', (req, res) => {
-    // res.sendStatus(400);
-    // res.status(400).json({ success: false });
+//#region Запросы
+//#region GET
+app.get('/api/v1/bootcamps', (req, res) => {
     res.status(200).json({
         success: true,
-        data: {
-            id: 1,
-            name: 'Roman',
-        },
+        message: 'Show all bootcamps',
     });
 });
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: `Show bootcamp ${req.params.id}`,
+    });
+});
+//#endregion
+
+//#region POST
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: 'Create new bootcamp',
+    });
+});
+//#endregion
+
+//#region PUT
+/* ID-это то, что находится :id
+ * и приходит в request.params.id*/
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: `Update bootcamp ${req.params.id}`,
+    });
+});
+//#endregion
+
+//#region DELETE
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: `Delete bootcamp ${req.params.id}`,
+    });
+});
+//#endregion
+
+//#endregion Запросы
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
     console.log(
         `Server running on -${process.env.NODE_ENV}- mode on port -${PORT}-`
