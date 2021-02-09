296a3f93c30e8e3499c6546d443d79dc085d987d
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-02-09 16:55:34 +0500

    3.3 Router. Responce. Отправляем данные по url /

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -6,6 +6,17 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+/* привзяка действий по маршруту localhost:5000/
+ * или просто localhost:5000*/
+app.get('/', (req, res) => {
+    /*Не надо вводить Headers key
+     * express сам определит к какому типу относиться*/
+
+    // res.send({ name: 'Brad' });
+    // res.json({ name: 'Brad' });
+    res.send('<h1>Hello from express</h1>');
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
