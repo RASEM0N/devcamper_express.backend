24cc57a9334aef20903e445dbb77d0df0f6d3960
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-02-09 22:38:28 +0500

    3.6 Add middleware Morgan (logger & npm i morgan)

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 // Middleware
-const logger = require('./middleware/logger.js');
+const morgan = require('morgan');
 // Router files
 const bootcamps = require('./routes/bootcamps.js');
 
@@ -11,7 +11,10 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.use(logger);
+// Dev loggin middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
