0ed39880ecc9d06487a68969e5656350975ebe33
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-02-10 20:38:13 +0500

    4.2 Connecting to The Database with Mongoose. [./config./db.js]

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -1,13 +1,18 @@
-// import
+//#region IMPORT
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db.js');
 // Middleware
 const morgan = require('morgan');
 // Router files
 const bootcamps = require('./routes/bootcamps.js');
+//#endregion *************************************
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connected to database
+connectDB();
 
 const app = express();
 
@@ -20,9 +25,18 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(
         `Server running on -${process.env.NODE_ENV}- mode on port -${PORT}-`
     );
+});
+
+// Handle unhandle promise rejections
+/* Отлавливает ошибки
+ * В случае ошибки выводит сообщение,
+ * а также закрывает сервер и обрывает процесс */
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
 });
