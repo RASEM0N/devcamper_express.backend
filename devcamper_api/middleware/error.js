d27cc7e80e18227d67fb7e6e10abdb47a8ae3728
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-02-12 00:44:25 +0500

    5.4 Mongoose Error Hading. Допиливание. Валидатор & Дубликаты

--- devcamper_api/middleware/error.js
+++ devcamper_api/middleware/error.js
@@ -5,12 +5,24 @@ const ErrorHandler = (err, req, res, next) => {
     error.message = err.message;
 
     // Log to console for dev
-    console.log(err.stack.red);
+    console.log(err);
 
     // Mongoose bad ObjectID
     if (err.name === 'CastError') {
         const message = `Bootcamp not found with id of ${err.value} `;
         error = new ErrorResponce(message, 404);
+    }
+
+    // Mongoose duplicate key
+    if (err.code === 11000) {
+        const message = `Diplicate filed value entered`;
+        error = new ErrorResponce(message, 400);
+    }
+
+    // Mongoose validation error
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map((item) => item.message);
+        error = new ErrorResponce(message, 400);
     }
 
     res.status(error.statusCode || 500).json({
