4e6e072caf65bd746f33c5cd250b3cce769216ec
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-02-13 18:11:29 +0500

    6.1 Seeder. Загрузка и удалениие bootcamps на серв

---
+++ devcamper_api/seeder.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const colors = require('colors');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Load models
+const Bootcamp = require('./models/Bootcamps');
+
+// Conntect to DB
+/* Подключаемся к серваку */
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+});
+
+// Read JSON files
+/* читаем данные из bootcamps.json и переводим в обьект */
+const bootcamp = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+// Import into DB
+/* Закидываем bootcamps.json в БД*/
+const importData = async () => {
+    try {
+        await Bootcamp.create(bootcamp);
+        console.log(`Data Imported...`.green.inverse);
+        process.exit();
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// Delete data
+/* Удаляем все из БД*/
+const deletetData = async () => {
+    try {
+        await Bootcamp.deleteMany();
+        console.log(`Data destroyes...`.red.inverse);
+        process.exit();
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+/* Запускаем в консоли через node seeder -i или -d.
+ * Все .pre остаются в рабочем состояние*/
+if (process.argv[2] === '-i') {
+    console.log(`-i`);
+    importData();
+} else if (process.argv[2] === '-d') deletetData();
